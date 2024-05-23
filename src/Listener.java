ef1821730a79f4aad92860d56cf82e922f0abc56
matrzask <maciejtrzask@student.agh.edu.pl>
2024-05-23 14:35:05 +0200

    better drawing function argument parsing

--- src/Listener.java
+++ src/Listener.java
@@ -12,7 +12,31 @@ public class Listener extends NewLogoParserBaseListener {
     @Override
     public void exitFunctionCall(NewLogoParser.FunctionCallContext ctx) {
         if(ctx.function().drawingFunction() != null) {
-            DrawingPanel.processCommand(panel, ctx.function().drawingFunction().getText(), ctx.callArguments().getText());
+            if(ctx.callArguments().getChildCount() == 0) {
+                System.err.println("No arguments for command!");
+                return;
+            }
+            else if(ctx.callArguments().getChildCount() > 1) {
+                System.err.println("Too many arguments for command!");
+                return;
+            }
+            String argument = "";
+            if(ctx.callArguments().getChild(0) instanceof NewLogoParser.ValueContext val) {
+                if(val.VARIABLE() != null) {
+                    // Get the value of the variable
+                }
+                else if(val.STRING_CONST() != null || val.CHAR_CONST() != null) {
+                    argument = val.getText();
+                    argument = argument.substring(1, argument.length()-1);
+                }
+                else {
+                    argument = val.getText();
+                }
+            }
+            
+            DrawingPanel.processCommand(panel, ctx.function().drawingFunction().getText(), argument);
         }
     }
+    
+    
 }
