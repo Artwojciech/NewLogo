958053b1d9cf30af46853a1b15e49b39d8e0acca
matrzask <maciejtrzask@student.agh.edu.pl>
2024-06-03 16:55:08 +0200

    Value get functions

--- src/Value.java
+++ src/Value.java
@@ -6,8 +6,8 @@ public class Value {
         BOOL
     }
     
-    private ValueType type;
-    private String value;
+    private final ValueType type;
+    private final String value;
     
     public Value(ValueType type, String value) {
         this.type = type;
@@ -20,5 +20,52 @@ public class Value {
     
     public String getValue() {
         return value;
+    }
+    
+    public int getInt () {
+        if (type != ValueType.INT) {
+            System.err.println("Value is not an integer!");
+            return 0;
+        }
+        
+        try {
+            return Integer.parseInt(value);
+        }
+        catch (NumberFormatException e) {
+            System.err.println("Value is not an integer!");
+            return 0;
+        }
+    }
+    
+    public String getString() {
+        if (type != ValueType.STRING) {
+            System.err.println("Value is not a string!");
+            return "";
+        }
+        
+        return value;
+    }
+    
+    public char getChar() {
+        if (type != ValueType.CHAR) {
+            System.err.println("Value is not a char!");
+            return '\0';
+        }
+        
+        if (value.length() > 1) {
+            System.err.println("Value is not a char!");
+            return '\0';
+        }
+        
+        return value.charAt(0);
+    }
+    
+    public boolean getBool() {
+        if (type != ValueType.BOOL) {
+            System.err.println("Value is not a boolean!");
+            return false;
+        }
+        
+        return Boolean.parseBoolean(value);
     }
 }
